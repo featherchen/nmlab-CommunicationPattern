24a59cb753153c9b65ea19de815283cf31567f05
featherchen <garychen0975321741@gmail.com>
2021-12-01 12:55:14 +0800

    add gRPC

--- mysite/tutorial/urls.py
+++ mysite/tutorial/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path, re_path
 
-from .views import EchoView1, EchoView2
+from .views import EchoView1, EchoView2, Fibonacci, Logging
 
 urlpatterns = [
-    re_path(r'^fibonacci/?$', EchoView1.as_view()),
-    re_path(r'^logs/?$', EchoView2.as_view()),
+    re_path(r'^fibonacci/?$', Fibonacci().as_view()),
+    re_path(r'^logs/?$', Logging.as_view()),
 ]
--- mysite/tutorial/views.py
+++ mysite/tutorial/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import permissions
+from google.protobuf.json_format import MessageToJson, MessageToDict
 
 # Create your views here.
 class EchoView1(APIView):
@@ -16,3 +17,71 @@ class EchoView2(APIView):
     def get(self, request):
         return Response(data={ 'echo': 'logs' }, status=200)
 
+class Fibonacci(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        import os
+        import os.path as osp
+        import sys
+        # BUILD_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "build/service/")
+        # sys.path.insert(0, BUILD_DIR)
+        GRPC_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "../../gRPC-with-protobuf/build/service/")
+        sys.path.insert(0, GRPC_DIR)
+        import argparse
+
+        import grpc
+        import fib_pb2
+        import fib_pb2_grpc
+
+        order=int(request.data["order"])
+        # host = f"{args['ip']}:{args['port']}"
+        # print(host)
+        host = "localhost:8080"
+        with grpc.insecure_channel(host) as channel:
+            stub = fib_pb2_grpc.FibCalculatorStub(channel)
+
+            req = fib_pb2.FibRequest()
+            req.order = order
+
+            response = stub.Compute(req)
+            # print(response.value)
+
+        return Response(data={ 'echo': response.value }, status=200)
+
+
+class Logging(APIView):
+    permission_classes = (permissions.AllowAny,)
+    
+    def get(self, request):
+        import os
+        import os.path as osp
+        import sys
+        # BUILD_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "build/service/")
+        # sys.path.insert(0, BUILD_DIR)
+        GRPC_DIR = osp.join(osp.dirname(osp.abspath(__file__)), "../../gRPC-with-protobuf/build/service/")
+        sys.path.insert(0, GRPC_DIR)
+        import argparse
+
+        import grpc
+        import fib_pb2
+        import fib_pb2_grpc
+
+        # order=int(request.data["order"])
+        # host = f"{args['ip']}:{args['port']}"
+        # print(host)
+        host = "localhost:8080"
+        with grpc.insecure_channel(host) as channel:
+            stub = fib_pb2_grpc.LoggerCalculatorStub(channel)
+
+            req = fib_pb2.LogRequest()
+            # req.order = order
+
+            response = stub.Log(req)
+            # print(response.value)
+            # print(response)
+            # serialized = MessageToJson(response)
+            response = MessageToDict(response, preserving_proto_field_name = True)
+            desired_res = response["history"]
+            history=[int(i) for i in desired_res]
+        return Response(data={ 'echo': history }, status=200)
